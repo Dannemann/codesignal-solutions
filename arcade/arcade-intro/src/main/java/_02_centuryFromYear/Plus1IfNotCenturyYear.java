edc493b57c48a78ea64e28353592f04bf4aaf5f7
Jean Dannemann Carone <jean.carone@gmail.com>
2020-08-30 01:45:53 -0300

    Replaced an if for a ternary if at arcade-intro._02_centuryFromYear.

--- arcade/arcade-intro/src/main/java/_02_centuryFromYear/Plus1IfNotCenturyYear.java
+++ arcade/arcade-intro/src/main/java/_02_centuryFromYear/Plus1IfNotCenturyYear.java
@@ -3,10 +3,7 @@ package _02_centuryFromYear;
 class Plus1IfNotCenturyYear extends AbstractSolution {
 
 	int centuryFromYear(int year) {
-		if (year % 100 == 0)
-			return year / 100;
-		else
-			return year / 100 + 1;
+		return year % 100 == 0 ? year / 100 : year / 100 + 1;
 	}
 
 }
