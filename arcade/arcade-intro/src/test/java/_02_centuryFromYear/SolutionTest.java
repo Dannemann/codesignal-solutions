090b705744925fda7b6778f7d46016b700ad4f96
Jean Dannemann Carone <jean.carone@gmail.com>
2020-08-29 22:46:00 -0300

    Added missing hidden tests to arcade-intro._02_centuryFromYear.

--- arcade/arcade-intro/src/test/java/_02_centuryFromYear/SolutionTest.java
+++ arcade/arcade-intro/src/test/java/_02_centuryFromYear/SolutionTest.java
@@ -22,10 +22,13 @@ public class SolutionTest {
 			assertEquals(17, s.centuryFromYear(1700));
 			assertEquals(20, s.centuryFromYear(1988));
 			assertEquals(20, s.centuryFromYear(2000));
+			assertEquals(21, s.centuryFromYear(2001));
 			assertEquals(2, s.centuryFromYear(200));
 			assertEquals(4, s.centuryFromYear(374));
 			assertEquals(1, s.centuryFromYear(45));
 			assertEquals(1, s.centuryFromYear(8));
+			// Hidden.
+			assertEquals(3, s.centuryFromYear(300));
 			assertEquals(16, s.centuryFromYear(1588));
 			assertEquals(1, s.centuryFromYear(1));
 			assertEquals(1, s.centuryFromYear(50));
