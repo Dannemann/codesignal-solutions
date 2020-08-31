af470e31a9feedd9bee08a9ccf263f85d930e545
Jean Dannemann Carone <jean.carone@gmail.com>
2020-08-31 12:10:58 -0300

    Removing decrement because we don't need var assignment, only result.

--- arcade/arcade-intro/src/main/java/_02_centuryFromYear/Subtract1DivideAdd1.java
+++ arcade/arcade-intro/src/main/java/_02_centuryFromYear/Subtract1DivideAdd1.java
@@ -3,7 +3,7 @@ package _02_centuryFromYear;
 class Subtract1DivideAdd1 extends AbstractSolution {
 
 	int centuryFromYear(int year) {
-		return --year / 100 + 1;
+		return (year - 1) / 100 + 1;
 	}
 
 }
