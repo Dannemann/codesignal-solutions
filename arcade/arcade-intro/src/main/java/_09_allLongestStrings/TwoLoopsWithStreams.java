f9dfb0e872d85a455ad5ca50e46e6cf6d34b602c
Jean Dannemann Carone <jean.carone@gmail.com>
2020-09-29 01:23:52 -0300

    Fixed arcade-intro._09_allLongestStrings.TwoLoopsWithStreams solution.

--- arcade/arcade-intro/src/main/java/_09_allLongestStrings/TwoLoopsWithStreams.java
+++ arcade/arcade-intro/src/main/java/_09_allLongestStrings/TwoLoopsWithStreams.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 class TwoLoopsWithStreams extends AbstractSolution {
 
 	String[] allLongestStrings(String[] inputArray) {
-		return Arrays.stream(inputArray).filter(s -> s.length() == Arrays.stream(inputArray).mapToInt(String::length).max().getAsInt()).toArray(String[]::new);
+		int longestLen = Arrays.stream(inputArray).mapToInt(String::length).max().getAsInt();
+
+		return Arrays.stream(inputArray).filter(s -> s.length() == longestLen).toArray(String[]::new);
 	}
 
 }
