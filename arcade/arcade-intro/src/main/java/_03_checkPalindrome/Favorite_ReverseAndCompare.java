8a7019cd2f454650f7118c12217955ab5101b663
Jean Dannemann Carone <jean.carone@gmail.com>
2023-08-08 17:36:11 -0300

    Enhancement on arcade-intro._03_checkPalindrome.Favorite_ReverseAndCompare.

--- arcade/arcade-intro/src/main/java/_03_checkPalindrome/Favorite_ReverseAndCompare.java
+++ arcade/arcade-intro/src/main/java/_03_checkPalindrome/Favorite_ReverseAndCompare.java
@@ -3,7 +3,7 @@ package _03_checkPalindrome;
 class Favorite_ReverseAndCompare extends AbstractSolution {
 
 	boolean checkPalindrome(String inputString) {
-		return inputString.equals(new StringBuilder(inputString).reverse().toString());
+		return inputString.contentEquals(new StringBuilder(inputString).reverse());
 	}
 
 }
