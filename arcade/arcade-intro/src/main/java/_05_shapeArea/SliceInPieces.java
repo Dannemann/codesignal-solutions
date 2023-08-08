f392b94492f5621e97f1aef46f34e09de38c7bdc
Jean Dannemann Carone <jean.carone@gmail.com>
2023-08-08 20:20:15 -0300

    Enhancements on arcade-intro._05_shapeArea.SliceInPieces.

--- arcade/arcade-intro/src/main/java/_05_shapeArea/SliceInPieces.java
+++ arcade/arcade-intro/src/main/java/_05_shapeArea/SliceInPieces.java
@@ -4,34 +4,20 @@ class SliceInPieces extends AbstractSolution {
 
 	/**
 	 * <p>
-	 * <b>To visualize this solution:</b> In this solution we slice the polygon in
-	 * pieces (blue, red, green, yellow and pink) as shown in the image below.
-	 * Notice how each piece is made of "<code>n * 2 - 1</code>" squares and we need
-	 * "<code>n - 1</code>" pieces to fill most of the polygon. To finish it, we
-	 * need one more piece of size "<code>n</code>" (the pink piece, which is the
-	 * initial value of "<code>n</code>"). To make it easier, try to visualize the
-	 * polygon as a square and not as a diamond.
+	 * <b>To visualize this solution:</b> In this solution, we slice the polygon
+	 * into pieces (blue, red, green, yellow, and pink), as shown in the image
+	 * below. Notice how each piece is made of "<code>(n * 2 - 1)</code>" squares,
+	 * and we need "<code>(n - 1)</code>" pieces to fill most of the polygon. To
+	 * finish it, we need one more piece of size "<code>n</code>" (the pink one). To
+	 * make it easier, try to visualize the polygon as a square and not as a
+	 * diamond.
 	 * </p>
 	 * <p>
 	 * <img src="assets/SliceInPieces_Ex.png">
 	 * </p>
 	 */
 	int shapeArea(int n) {
-		int nMinus1 = n - 1;
-		int piece = n * 2 - 1;
-
-		for (int i = 0; i < nMinus1; i++)
-			n += piece;
-
-		return n;
+		return (n * 2 - 1) * (n - 1) + n;
 	}
-
-	/**
-	 * IntStream version.
-	 */
-//	int shapeArea(int n) {
-//		int piece = n * 2 - 1;
-//		return IntStream.range(0, n - 1).map(i -> piece).sum() + n;
-//	}
 
 }
